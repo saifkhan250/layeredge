86b07a64f9a8a7f486c46c5ef2da20155c4b8d67
saifkhan250 <saifkhan250@gmail.com>
2025-01-30 10:21:52 +0500

    Add files via upload

---
+++ config/banner.js
@@ -0,0 +1,5 @@
+export function getBanner() {
+  const banner = `Join Here For More Script And Updates : (https://t.me/D4rkCipherX)
+`;
+  return banner;
+}
---
+++ utils/config.js
@@ -0,0 +1,4 @@
+export const config = {
+  ref_code: "RrzuHRWn",
+  num_ref: 100, // number of refer do you want 
+};
